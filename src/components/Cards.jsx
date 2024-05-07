dc0c24105eb1e645918d4fe6fbae5f6d3576c730
Angel Bernal <144516945+AngelBernaal@users.noreply.github.com>
2024-05-06 20:08:53 -0600

    Segunda Version

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,19 @@
-import Card from "./Card";
+import { Players } from "./Players";
 import "../main.css"
 
 export default function Cards(){
+
+    const section = Players.map(player => 
+        <div key={Players.id} className="card">
+        <img src={player.image} alt={player.name} />
+        <div className="info">
+        <h2>{player.name}</h2>
+        <p className="card__team">{player.position}{" | "}{ <img src={player.team} alt={player.name} />}</p>
+        <p className='desc'>{player.description}</p>
+        </div>
+        </div>
+    );
+
     return(
         <>
 
@@ -11,45 +23,7 @@ export default function Cards(){
         </div>
 
         <div className="grid">
-        <Card 
-        image="../../public/images/1.jpg"
-        name="Patrick Mahomes"
-        position="QB"
-        team="../../public/images/kansascity.png"
-        description="Con su habilidad para lanzar pases precisos y su capacidad para improvisar, Mahomes ha demostrado ser uno de los mejores quarterbacks de la liga."
-        />
-
-        <Card 
-        image="../../public/images/2.jpg"
-        name="Joe Burrow"
-        position="QB"
-        team="../../public/images/bengals.png"
-        description="Su habilidad para leer las defensas y su precisión en el pase lo convierten en uno de los quarterbacks más emocionantes de la liga."
-        />
-
-        <Card 
-        image="../../public/images/3.jpg"
-        name="Josh Allen"
-        position="QB"
-        team="../../public/images/bills.png"
-        description="Su capacidad para lanzar pases profundos y correr el balón lo hace una amenaza constante para las defensas rivales."
-        />
-
-        <Card 
-        image="../../public/images/4.jpg"
-        name="Justin Jefferson"
-        position="WR"
-        team="../../public/images/vikings.png"
-        description="Su habilidad para crear separación y atrapar pases difíciles lo convierte en uno de los receptores más prometedores de la liga. "
-        />
-
-        <Card 
-        image="../../public/images/5.jpg"
-        name="Christian McCaffrey"
-        position="RB"
-        team="../../public/images/niners.png"
-        description="Con una combinación única de velocidad, agilidad y visión en el campo, McCaffrey es una pieza clave."
-        />
+        {section}
         </div>
         </>
     )
---
+++ src/components/Players.jsx
@@ -0,0 +1,92 @@
+export const Players = [{
+        id: 0,
+        image : "../../public/images/1.jpg",
+        name: "Patrick Mahomes",
+        position: "QB",
+        team: "../../public/images/kansascity.png",
+        description: "Su habilidad para leer las defensas y su precisión en el pase lo convierten en uno de los quarterbacks más emocionantes de la liga."
+    },
+
+    {
+        id: 1,
+        image : "../../public/images/2.jpg",
+        name: "Joe Burrow",
+        position: "QB",
+        team: "../../public/images/bengals.png",
+        description: "Su habilidad para leer las defensas y su precisión en el pase lo convierten en uno de los quarterbacks más emocionantes de la liga."
+    
+    },
+
+    {
+        id: 2,
+        image : "../../public/images/3.jpg",
+        name: "Josh Allen",
+        position: "QB",
+        team: "../../public/images/bills.png",
+        description: "Su capacidad para lanzar pases profundos y correr el balón lo hace una amenaza constante para las defensas rivales."
+    },
+
+    {
+        id: 3,
+        image : "../../public/images/4.jpg",
+        name: "Justin Jefferson",
+        position: "WR",
+        team: "../../public/images/vikings.png",
+        description: "Su habilidad para crear separación y atrapar pases difíciles lo convierte en uno de los receptores más prometedores de la liga. "
+    },
+
+    {
+        id: 4,
+        image : "../../public/images/5.jpg",
+        name: "Christian McCaffrey",
+        position: "RB",
+        team: "../../public/images/niners.png",
+        description: "Con una combinación única de velocidad, agilidad y visión en el campo, McCaffrey es una pieza clave."
+    },
+
+    {
+        id: 5,
+        image: "../../public/images/6.jpg",
+        name: "Travis Kelce",
+        position: "TE",
+        team: "../../public/images/kansascity.png",
+        description: "Un ala cerrada dominante con habilidades de recepción similares a las de un receptor, mejorando el puntaje."
+    },
+
+    {
+        id: 6,
+        image: "../../public/images/7.jpg",
+        name: "T.J. Watt",
+        position: "EDGE",
+        team: "../../public/images/pitts.png",
+        description: "Con una combinación única de velocidad, agilidad y visión en el campo, Watt es un cazamariscales dominante."
+    },
+
+    {
+        id: 7,
+        image: "../../public/images/7.png",
+        name: "Nick Bosa",
+        position: "EDGE",
+        team: "../../public/images/niners.png",
+        description: "Bosa es un jugador joven con un gran potencial, capaz de presionar al mariscal de campo y detener la carrera con facilidad."
+    },
+
+    {
+        id: 8,
+        image: "../../public/images/8.png",
+        name: "George Kittle",
+        position: "TE",
+        team: "../../public/images/niners.png",
+        description: "Kittle es un tight end dinámico que puede bloquear y atrapar pases con la misma eficacia, proporcionando una gran ventaja para su equipo en el juego aéreo y terrestre."
+    },
+
+    {
+        id: 9,
+        image: "../../public/images/8.jpg",
+        name: "Myles Garrett",
+        position: "EDGE",
+        team: "../../public/images/clev.png",
+        description: "Garrett es un cazamariscales físico con una explosividad única, capaz de derribar al mariscal de campo con facilidad."
+    }
+    
+];
