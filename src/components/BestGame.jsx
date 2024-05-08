88490f92f931085c78deb66c5077e4bf89d40ea0
Angel Bernal <144516945+AngelBernaal@users.noreply.github.com>
2024-05-08 15:40:39 -0600

    Segunda Version

--- src/components/BestGame.jsx
+++ src/components/BestGame.jsx
@@ -1,20 +1,16 @@
 export const BestGame = [{
     image: "/image1.png",
-    team: "/kansascity.png"
     },
 
     {
     image: "/image2.png",
-    team: "/niners.png"
     },
 
     {
     image: "/image3.png",
-    team: "/bills.png"
     },
 
     {
     image: "/image4.png",
-    team: "/bengals.png"
     }
 ]
