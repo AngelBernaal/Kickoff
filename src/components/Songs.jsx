b3047ba6d38cca0562761ec4edc21c6c7422d9b3
Angel Bernal <144516945+AngelBernaal@users.noreply.github.com>
2024-05-10 18:29:50 -0600

    Segunda Version

--- src/components/Songs.jsx
+++ src/components/Songs.jsx
@@ -10,4 +10,16 @@ export const Song = [{
     ]
     },
 
+    {
+        id:2,
+        name: "NOT ALL HEROES WEAR CAPES",
+        artista: "Metro Boomin",
+        img: "/album2.jpg",
+        enlaces: [{id:1, link:"https://open.spotify.com/intl-es/album/1zNr37qd3iZJ899byrTkcj", boton: "/spotify.png"},
+               {id: 2, link: "https://music.apple.com/us/album/not-all-heroes-wear-capes/1441065529", boton: "/music.png"},
+               {id: 3, link: "https://music.youtube.com/playlist?list=OLAK5uy_m_qhVhF_dV-U_fu9FhN3G7Pf7DoBbiC6k", boton: "/video.png"},
+               {id: 4, link: "https://www.deezer.com/en/album/77438442", boton: "/deezer.png"},
+        ]
+        }
+
 ]
